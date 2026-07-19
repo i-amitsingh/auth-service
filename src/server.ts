af5b48778adb245e8fab3592149660d114a12657
Amit Singh <amitmansingh2003@gmai.com>
2026-07-19 14:58:06 +0530

    add express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-// import { Config } from './config/index.ts';
+import { Config } from './config/index.ts';
+import { app } from './app.ts';
+
+const startServer = () => {
+    try {
+        const port = Config.PORT;
+
+        app.listen(port, () => {
+            // eslint-disable-next-line no-console
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error('Error starting the server:', error);
+    }
+};
+
+startServer();
